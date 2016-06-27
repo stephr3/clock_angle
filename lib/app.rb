a27480843ce4605c995f29be379a5a4b84d2f41e
Stephanie Gurung and Tony Fabery <tony@>
2016-06-27 13:44:25 -0700

    returns the smaller angle between hour and minute hands

--- lib/app.rb
+++ lib/app.rb
@@ -2,6 +2,14 @@ class String
   define_method(:clock_angle) do
     time_array = self.split(":")
     hour = time_array[0].to_i
-    hour.*(30)
+    minute = time_array[1].to_i
+    hour_angle = (hour.*(30)).+(minute.*(0.5))
+    minute_angle = minute.*(6)
+    if (hour_angle - minute_angle) > 180
+      angle = 360 - (hour_angle - minute_angle)
+    else
+      angle = hour_angle - minute_angle
+    end
+    angle
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -5,4 +5,10 @@ describe('String#clock_angle') do
   it('returns an angle for the hour hand') do
     expect(("1:00").clock_angle()).to(eq(30))
   end
+  it('returns the angle between hour and minute hands') do
+    expect(("12:30").clock_angle()).to(eq(165))
+  end
+  it('returns the smaller angle between hour and minute hands') do
+    expect(("12:50").clock_angle()).to(eq(85))
+  end
 end
